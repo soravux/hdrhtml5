e454ba32e22bd77ee63ae992aaf6dafc023d1829
Yannick Hold <yannickhold@gmail.com>
2015-05-10 15:33:38 -0400

    Added arbitrary size input

--- gethdr.py
+++ gethdr.py
@@ -1,10 +1,14 @@
-from hdrio import imread
+import sys
 import json
+
 import numpy as np
 import scipy.ndimage
+from hdrio import imread
 
-image = imread('envmap.exr')
-imresized = scipy.ndimage.interpolation.zoom(image, (0.15, 0.15, 1), order=1)
+image = imread(sys.argv[1])
+print(image.shape)
+imresized = scipy.ndimage.interpolation.zoom(image, (580./image.shape[0], 580/image.shape[1], 1), order=1)
+print(imresized.shape)
 alpha = np.ones(imresized.shape[0:2])
 imresizeda = np.dstack((imresized, alpha))
 out = imresizeda
