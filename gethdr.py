3c50f917aae86e6fd9c0f9ae3fc000251bf78866
Yannick Hold <yannickhold@gmail.com>
2015-05-11 02:22:04 -0400

    Removed extraneous cmoments

--- gethdr.py
+++ gethdr.py
@@ -6,14 +6,10 @@ import scipy.ndimage
 from hdrio import imread
 
 image = imread(sys.argv[1])
-print(image.shape)
 imresized = scipy.ndimage.interpolation.zoom(image, (580./image.shape[0], 580/image.shape[1], 1), order=1)
-print(imresized.shape)
 alpha = np.ones(imresized.shape[0:2])
 imresizeda = np.dstack((imresized, alpha))
 out = imresizeda
-print(out[:3,:3,:])
-print(out.flatten()[:20])
 with open('targetData.js', 'w') as fhdl:
     fhdl.write("var targetData = ")
     json.dump(out.flatten().tolist(), fhdl)
